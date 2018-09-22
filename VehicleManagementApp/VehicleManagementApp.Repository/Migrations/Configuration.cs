a5d6549715e4494b708030fc9533deedd5ed5b4c
ziaultalukder <talukderziaul2@gmail.com>
2018-09-22 14:06:45 +0600

    RequsitionIndex Table Some Change

---
+++ VehicleManagementApp/VehicleManagementApp.Repository/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace VehicleManagementApp.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VehicleManagementApp.Repository.DatabaseContext.VehicleDatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VehicleManagementApp.Repository.DatabaseContext.VehicleDatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
