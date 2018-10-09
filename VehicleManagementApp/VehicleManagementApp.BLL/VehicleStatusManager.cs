5fa781f91c33fdf92524b89e36a02f5bb50fa513
KaziShalauddin <shalauddin99@gmail.com>
2018-10-09 18:45:09 +0600

    DriverStatus and VehicleStatus all files for Solid Pattern added.

---
+++ VehicleManagementApp/VehicleManagementApp.BLL/VehicleStatusManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleManagementApp.BLL.Base;
+using VehicleManagementApp.BLL.Contracts;
+using VehicleManagementApp.Models.Models;
+using VehicleManagementApp.Repository;
+using VehicleManagementApp.Repository.Contracts;
+using VehicleManagementApp.Repository.Repository;
+
+namespace VehicleManagementApp.BLL
+{
+    public class VehicleStatusManager:Manager<VehicleStatus>,IVehicleStatusManager
+    {
+        private IVehicleStatusRepository vehicleStatusRepository;
+
+        public VehicleStatusManager() : base(new VehicleStatusRepository())
+        {
+            this.vehicleStatusRepository = (VehicleStatusRepository)base.BaseRepository;
+        }
+        public VehicleStatusManager(IVehicleStatusRepository Repository) : base(Repository)
+        {
+            this.vehicleStatusRepository = Repository;
+        }
+
+       
+
+       
+    }
+}
---
+++ VehicleManagementApp/VehicleManagementApp.Repository/DriverStatusRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleManagementApp.Models.Models;
+using VehicleManagementApp.Repository.Contracts;
+using VehicleManagementApp.Repository.Repository;
+
+namespace VehicleManagementApp.Repository
+{
+    public class DriverStatusRepository:DeletableRepository<DriverStatus>, IDriverStatusRepository
+    { 
+
+    }
+}
